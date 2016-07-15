3ec50df93666babe49dfe95942a281d6f2e07580
Ray Walters <rlwalters3@yahoo.com>
2016-07-15 10:28:32 -0400

    messing with submit button

--- script.js
+++ script.js
@@ -1,16 +1,15 @@
 
 $(function(){
-	//hover
-	var $seat = $("div.col-md-3");
+
+	//hover to fade button
+	var $seat = $("button.button");
 	$seat.hover(function(){
 		$(this).fadeTo("fast", .5);
 	},function(){
 		$(this).fadeTo("fast", 1);
 	});
 
-	//click
-
-
+	//click to toggle the blue class
 	$seat.click(function(){
 		$(this).toggleClass("blue");
 		//$(this).unbind("onmouseover").unbind("onmouseout");
@@ -19,14 +18,27 @@ $(function(){
 
 $(".button").on('click',function() {
   var text = $(this).text();
-  $("input").val(text);
+  $("#input").val(text);
 });
 
 
 
-$(".button").on('click', function(){
-	$(this).text("Reserved");
-})
+// $(".button").on('click', function(){
+// 	$(this).text("Reserved");
+// });
+
+//******Check that fields are filled in to submit******
+$("button").click(function(){
+	var $name = $("input#enterName");
+	var $email = $("input#enterEmail");
+	var $date = $("input#selectDate");
+	var $allSeats = $("input#input");
+	if($name.text() === " " && $email.text() === " " && $date.text() === " " && $allSeats.text() === " "){
+		console.log("nope");
+	}else{
+		console.log("Booked");
+	}
+});
 
 
 
